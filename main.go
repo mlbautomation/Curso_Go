1c6d542c987d1c1238410fb2f86af633ccb61e2b
Marlon Ly <111965772+mlbautomation@users.noreply.github.com>
2024-10-25 14:05:52 -0500

    only chage in main

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	servidorECHO5 "github.com/mlbautomation/Curso_GO/04-Go-Server/05-Servidor-ECHO"
-)
+import servidorECHO3 "github.com/mlbautomation/Curso_GO/04-Go-Server/03-Servidor-ECHO"
 
 func main() {
 
 	//Read Notes.txt
-	servidorECHO5.ServidorJWT_ECHO()
+	servidorECHO3.ServerCertificate()
 }
